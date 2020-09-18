c44d3991b2ad19f3ce4d3ba9de73735062367dd7
Opilane TTHK <opilane@tthk.ee>
2020-09-18 13:20:59 +0300

    Add project files.

---
+++ ButenkoLOGITpv19/ButenkoLOGITpv19/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ButenkoLOGITpv19
+{
+    public partial class MainPage : ContentPage
+    {
+        private object isVisible;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public object Yellow { get; private set; }
+        public object Green { get; private set; }
+
+        private async void On_Clicked(object sender, EventArgs e)
+        {
+            bool vkl = true;
+            while (vkl != false)
+            {
+                h.BackgroundColor = Color.Red;
+                b.BackgroundColor = Color.Aqua;
+                f.BackgroundColor = Color.Aqua;
+                await Task.Run(() => System.Threading.Thread.Sleep(15));
+                for (int i = 0; i < 3; i++)
+                {
+                    h.BackgroundColor = Color.Black;
+                    b.BackgroundColor = Color.Red;
+                    f.BackgroundColor = Color.Aqua;
+                    await Task.Run(() => System.Threading.Thread.Sleep(15));
+                }
+
+                h.BackgroundColor = Color.Black;
+                b.BackgroundColor = Color.Aqua;
+                f.BackgroundColor = Color.Red;
+                await Task.Run(() => System.Threading.Thread.Sleep(15));
+            }
+
+            private void off_Clicked(object sender, EventArgs e)
+            {
+                h.BackgroundColor = isVisible;
+                b.BackgroundColor = isVisible;
+                fBackgroundColor = isVisible;
+                vkl = false;
+
+
+            }
+        }
+    }
+}
+
+
+
