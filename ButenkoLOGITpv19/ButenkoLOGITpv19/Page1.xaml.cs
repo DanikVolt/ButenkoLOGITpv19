70302b9d4358326f302b7af5d92db4ebb35b9149
opilane <opilane@HP-CZC6297B9D>
2020-09-28 15:44:08 +0300

    fdgdgf

--- ButenkoLOGITpv19/ButenkoLOGITpv19/Page1.xaml.cs
+++ ButenkoLOGITpv19/ButenkoLOGITpv19/Page1.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Threading;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,7 +12,8 @@ namespace ButenkoLOGITpv19
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Page1 : ContentPage
     {
-        BoxView pea, keha, keha2, keha3, kamen, glaz1, glaz2, gl, sh;
+        BoxView pea, keha, keha2, keha3, kamen, glaz1, glaz2, gl, sh, r, r1;
+        Button b1, b2;
         public Page1()
         {
             AbsoluteLayout abs = new AbsoluteLayout();
@@ -63,7 +64,101 @@ namespace ButenkoLOGITpv19
             AbsoluteLayout.SetLayoutFlags(sh, AbsoluteLayoutFlags.PositionProportional);
             abs.Children.Add(sh);
 
+            r = new BoxView { Color = Color.Black, CornerRadius = 10 };
+            AbsoluteLayout.SetLayoutBounds(r, new Rectangle(0.25, 0.4, 65, 10));
+            AbsoluteLayout.SetLayoutFlags(r, AbsoluteLayoutFlags.PositionProportional);
+            abs.Children.Add(r);
+
+            r1 = new BoxView { Color = Color.Black, CornerRadius = 10 };
+            AbsoluteLayout.SetLayoutBounds(r1, new Rectangle(0.75, 0.4, 65, 10));
+            AbsoluteLayout.SetLayoutFlags(r1, AbsoluteLayoutFlags.PositionProportional);
+            abs.Children.Add(r1);
+
+            b1 = new Button {Text="Холоп"};
+            b1.Clicked += B1_Clicked;
+            AbsoluteLayout.SetLayoutBounds(b1, new Rectangle(0.3, 0.8, 70, 50));
+            AbsoluteLayout.SetLayoutFlags(b1, AbsoluteLayoutFlags.PositionProportional);
+            abs.Children.Add(b1);
+            b2 = new Button { Text = "Таять" };
+            b2.Clicked += B2_Clicked;
+            AbsoluteLayout.SetLayoutBounds(b2, new Rectangle(0.7, 0.8, 70, 50));
+            AbsoluteLayout.SetLayoutFlags(b2, AbsoluteLayoutFlags.PositionProportional);
+            abs.Children.Add(b2);
+
             Content = abs;
         }
+
+        private async void B2_Clicked(object sender, EventArgs e)
+
+        {
+            if (flag)
+            {
+                pea.Opacity = 0;
+                glaz1.Opacity = 0;
+                glaz2.Opacity = 0;
+                gl.Opacity = 0;
+                keha3.Opacity = 0;
+                kamen.Opacity = 0;
+                sh.Opacity = 0;
+                keha.BackgroundColor = Color.Gold;
+                keha2.BackgroundColor = Color.Gold;
+                await Task.Run(() => System.Threading.Thread.Sleep(300));
+                r1.Opacity = 0;
+                r.Opacity = 0;
+                pea.Opacity = 0;
+                keha.Opacity = 0;
+                keha2.BackgroundColor = Color.Gold;
+                await Task.Run(() => System.Threading.Thread.Sleep(300));
+                pea.Opacity = 0;
+                keha.Opacity = 0;
+                keha2.Opacity = 0;
+                await Task.Run(() => System.Threading.Thread.Sleep(300));
+
+
+            }
+
+        }
+
+        bool flag = true;
+        private void B1_Clicked(object sender, EventArgs e)
+
+        {
+            if (flag)
+            {
+                pea.Color = Color.White;
+                keha.Color = Color.White;
+                keha2.Color = Color.White;
+                sh.Color = Color.Black;
+                keha3.Color = Color.Black;
+                kamen.Color = Color.Black;
+                glaz1.Color = Color.Black;
+                glaz2.Color = Color.Black;
+                flag = false;
+
+            }
+
+            else
+            {
+                pea.Color = Color.Gold;
+                keha.Color = Color.Gold;
+                keha2.Color = Color.Gold;
+                sh.Color = Color.Red;
+                keha3.Color = Color.Red;
+                kamen.Color = Color.DarkBlue;
+                glaz1.Color = Color.Red;
+                glaz2.Color = Color.Red;
+                flag = true;
+
+            }
+
+        }
+
+ 
+
+        
+
+
+
     }
+
 }
